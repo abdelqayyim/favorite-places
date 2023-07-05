aa49f126802d3c8a255e302585eb95a3c3df0f87
abdelqayyim <abdelymaster@gmail.com>
2023-07-04 22:18:55 -0400

    Added pick on map with marker functionality

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,82 +1,127 @@
-import { Alert, View, Text, StyleSheet, Image } from "react-native";
-import OutlinedButton from "../UI/OutlinedButton";
-import { Colors } from "../../constants/colors";
-import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
-import { useState } from "react";
-import { getMapPreview } from "../../util/location";
-import { useNavigation } from "@react-navigation/native";
-const LocationPicker = (props) => {
-    const [pickedLocation, setPickedLocation] = useState();
-    const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
-    const navigation = useNavigation();
+import { useEffect, useState } from 'react';
+import { Alert, View, StyleSheet, Image, Text } from 'react-native';
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from 'expo-location';
+import {
+  useNavigation,
+  useRoute,
+  useIsFocused,
+} from '@react-navigation/native';
 
+import { Colors } from '../../constants/colors';
+import OutlinedButton from '../UI/OutlinedButton';
+import { getMapPreview } from '../../util/location';
 
-    async function verifyPermissions() {
-        if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-            const permissionResponse = await requestPermission();
-            return permissionResponse.granted;
-        }
-        if (locationPermissionInformation.status === PermissionStatus.DENIED) {
-            Alert.alert('Insufficient Permissions!', "You need to grant location permissions to use this app");
-            return false;
-        }
-        return true;
+function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+  const isFocused = useIsFocused();
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      const mapPickedLocation = {
+        lat: route.params.pickedLat,
+        lng: route.params.pickedLng,
+      };
+      setPickedLocation(mapPickedLocation);
     }
-    async function getLocationHandler() {
-        const hasPermission = await verifyPermissions();
-        if (!hasPermission) {
-            return;
-        }
-        const location = await getCurrentPositionAsync();
-        setPickedLocation({
-            lat: location.coords.latitude,
-            lng: location.coords.longitude
-        });
+  }, [route, isFocused]);
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
     }
-    function pickOnMapHandler() {
-        navigation.navigate('Map');
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant location permissions to use this app.'
+      );
+      return false;
     }
-    let locationPreview = <Text>No Location Picked Yet.</Text>;
-    if (pickedLocation) {
-        locationPreview = <Image
-            style={styles.image}
-            source={{
-                uri: getMapPreview(pickedLocation.lat, pickedLocation.lng)
-            }}
-        />;
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
     }
-    return (
-        <View>
-            <View style={styles.mapPreview}>
-            {locationPreview}
-            </View>
-            <View style={styles.actions}>
-                <OutlinedButton icon='location' onPress={getLocationHandler}>Locate User</OutlinedButton>
-                <OutlinedButton icon='map' onPress={pickOnMapHandler}>Pick on Map</OutlinedButton>
-            </View>
-        </View>
-    )
-};
+
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  }
+
+  function pickOnMapHandler() {
+    navigation.navigate('Map');
+  }
+
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
 
 export default LocationPicker;
+
 const styles = StyleSheet.create({
-    mapPreview: {
-        width: '100%',
-        height: 200,
-        marginVertical: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.primary100,
-        borderRadius: 4,
-        overflow: 'hidden'
-    },
-    actions: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-    }
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+    overflow: 'hidden',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    // borderRadius: 4
+  },
 });
